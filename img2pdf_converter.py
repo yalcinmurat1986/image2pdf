105b4a3a5927f361558c22a3840de68509129bed
Murat Yalcin <yalcinmurat1986@gmail.com>
2019-05-19 18:33:46 -0700

    Add files via upload

---
+++ img2pdf_converter.py
@@ -0,0 +1,27 @@
+import img2pdf
+import argparse
+
+
+def img2pdf_converter(images, path, save = False):
+
+    pdf_result = img2pdf.convert(images)
+
+    if save:
+        with open("img2pdf.pdf","wb") as f:
+            f.write()
+
+
+    return pdf_result
+
+
+
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--images', required=True, help = 'list of image names')
+    parser.add_argument('--path', required=False, type = str, help = 'path to save pdf')
+    parser.add_argument('--save', dest='save', action='store_true')
+    parser.set_defaults(save=False)
+    args = parser.parse_args()
+    img2pdf_converter(args.images, args.path, args.save)
